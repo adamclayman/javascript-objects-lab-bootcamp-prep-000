1db2297a7854c323365a02246ab7ca3ede73c96a
Adam Clayman <adam@clayman.org>
2017-09-28 16:53:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+recipes = {};
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign(obj, {key: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey() {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
