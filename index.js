50ce6b61b561191384299bc2057a078787f4871d
Adam Clayman <adam@clayman.org>
2017-09-28 17:18:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  var newObj = Object.assign(obj, {});
-  newObj[key] = value;
+  var newObj = Object.assign(obj, {[key]: value});
   return newObj;
 }
 
